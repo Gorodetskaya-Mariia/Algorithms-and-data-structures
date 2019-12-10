0e3d68452654a380114a9a03521766708a9b7b4c
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-10 18:36:03 +0300

    reverse a number has been done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
+function reverseInt(n) {
+	const result = n.toString().split("").reverse().join("");
+	return parseInt(result) * Math.sign(n);
+}
+//I can not use Number(val) instead of parseInt(val) due to parseInt ignores "-" at the end of string and return just a number
+//Math.sign(20) == 1, Math.sign(-20) == -1
 module.exports = reverseInt;

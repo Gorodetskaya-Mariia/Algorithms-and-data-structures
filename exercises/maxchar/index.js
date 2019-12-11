06b93113f5c4a43a387bd9ce273498843994ccb7
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-11 16:44:21 +0300

    maxchar has been done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let obj = {};
+
+	for(let char of str){
+		if (obj[char]){
+			obj[char]++;
+		} else {
+			obj[char] = 1;
+		}
+	}
+	const max = Math.max(...Object.values(obj));
+	for(let key in obj){
+		if(obj[key] == max)
+		return key;
+	}
+}
 
 module.exports = maxChar;

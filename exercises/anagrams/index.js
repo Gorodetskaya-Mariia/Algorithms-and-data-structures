41fd36453e41b464b6d8472363cfccfc2299642a
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-24 16:38:32 +0300

    anagrams have been solved

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,64 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB){
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str){
+	return str.replace(/[^\w]/g,"").toLowerCase().split("").sort().join("");
+}
+//second mode
+// function anagrams(stringA, stringB) {
+// 	const objA = buildObj(stringA);
+// 	const objB = buildObj(stringB);
+
+// 	if(Object.keys(objA).length !== Object.keys(objB).length){
+// 		return false;
+// 	}
+
+// 	for(let char in objA){
+// 		if(objA[char] !== objB[char]){
+// 			return false;
+// 		}
+// 	}
+
+// 	return true;
+// }
+
+// function buildObj(str){
+// 	let newObj = {};
+// 	for(let char of str.replace(/[^\w]/g).toLowerCase()){
+// 		newObj[char] = newObj[char] + 1 || 1;
+// 	}
+
+// 	return newObj;
+// }
 
 module.exports = anagrams;
+
+
+//from Yandex, write a function getAnagrams(“нос”, “сон”, “снедь”, “днесь”)
+//the result should be 
+// [
+//   ["нос", "сон"],
+//   ["днесь", "снедь"]
+// ]
+
+// function getAnagrams(...array){
+// 	let result = [];
+// 	array.forEach((item, index) => {
+// 		let newArray =[];
+// 		for(let i=index; i < array.length; i++){
+// 			if(cleanString(item) === cleanString(array[i])){
+// 				newArray.push(array[i])
+// 			}
+// 		}
+// 		if(newArray.length > 1) result.push(newArray);
+// 	});
+// 	return result;
+// }
+
+// function cleanString(str){
+// 	return str.split("").sort().join("");
+// }

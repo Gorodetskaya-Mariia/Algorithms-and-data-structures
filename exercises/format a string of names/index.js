3ad673d8bb670ead4b067a9e280aeb97724bd1da
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-24 16:45:46 +0300

    two modes for formating a string of names have been added

--- exercises/format a string of names/index.js
+++ exercises/format a string of names/index.js
@@ -30,4 +30,26 @@ function listNames(names) {
   }, "");
 }
 
+//second mode
+// function list(names){
+//   return names.reduce(function(prev, current, index, array){
+//     if (index === 0){
+//       return current.name;
+//     }
+//     else if (index === array.length - 1){
+//       return prev + ' & ' + current.name;
+//     } 
+//     else {
+//       return prev + ', ' + current.name;
+//     }
+//   }, '');
+//  }
+
+//the third mode
+// function list(names) {
+//   var xs = names.map(p => p.name)
+//   var x = xs.pop()
+//   return xs.length ? xs.join(", ") + " & " + x : x || ""
+// }
+
 module.exports = listNames;

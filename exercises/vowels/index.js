bc290d562b5c7da1f962cb58d300718e7c7a625a
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-26 15:11:23 +0300

    counting vowels has been added

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,33 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//1
+function vowels(str) {
+	let count = 0;
+	str.toLowerCase().split("").forEach(char => {
+		char === "a" || char === "e" || char === "i" || char === "o" || char === "u" ? count++ : count;
+	});
+	return count
+}
 
+//2
+function vowels(str) {
+	return str.toLowerCase().replace(/[^a,e,i,o,u]/gi,"").length;
+}
+
+//3
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
+}
+
+//4
+function vowels(str) {
+	const array = ["a","e","i","o","u"];
+	let count = 0;
+	str.toLowerCase().split("").forEach((char)=>{
+		array.includes(char) ? count++ : count;
+	});
+	return count;
+}
 module.exports = vowels;

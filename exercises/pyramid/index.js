7468607c86022f76e4d59a6ba52a60182d30a997
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-01-06 13:56:01 +0300

    pyramid has been solved

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,56 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let columns = 2 * n - 1;
+  let level = n;
+
+  while (level > 0) {
+    let result = Array(columns).fill(" ");
+
+    for (let i = level - 1; i <= columns - level; i++) {
+      result[i] = "#";
+    }
+    level--;
+    console.log(result.join(""));
+  }
+}
+
+//second mode - using midpoint
+function pyramid(n) {
+	const midpoint = Math.floor((2*n - 1) /2);
+	for(let row = 0; row < n; row++){
+		let level = "";
+		for(let column = 0; column < 2*n - 1; column++){
+			if(midpoint - row <= column && midpoint + row >= column){
+				level+= "#"
+			} else {
+				level+=" "
+			}
+		}
+		console.log(level);
+	}
+}
+
+//third mode - using recursion
+function pyramid(n, row = 0, level = "") {
+	if(row === n){
+		return;
+	}
+
+	if(level.length === 2*n - 1){
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+
+	const midpoint = Math.floor((2*n - 1)/2);
+	if(midpoint - row <= level.length && midpoint + row >= level.length){
+		level+= "#"
+	} else {
+		level+= " "
+	}
+
+	pyramid(n, row, level);
+}
 
 module.exports = pyramid;

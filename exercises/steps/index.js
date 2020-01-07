c4f0a2e0c551215be2737b02dbc3131b60248197
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-01-07 15:33:16 +0300

    steps has been solved

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	let level = 1;
+	while(level <= n){
+		let str = Array(n).fill(" ");
+		for(let i=0; i < level; i++){
+			str[i] = "#"
+		}
+		console.log(str.join(""));
+		level++;
+	}
+}
+
+//second mode
+function steps(n) {
+	for(let row = 1; row <= n; row++){
+		let str = "";
+		for(let columns = 0; columns < n; columns++){
+			if(columns < row){
+				str+= "#"
+			} else {
+				str+= " "
+			}
+		}
+		console.log(str);
+	}
+}
+
+//third mode using recursion
+function steps(n, row = 0, str = "") {
+	if(n === row)return;
+
+	if(n === str.length){
+		console.log(str);
+		return steps(n, row + 1);
+	}
+
+	if(str.length <= row){
+		str+= "#"
+	} else {
+		str+= " "
+	}
+	steps(n, row, str);
+}
 
 module.exports = steps;

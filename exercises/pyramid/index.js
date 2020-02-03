945e32c0189bfb6bedede3893b0252f258420287
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-02-03 18:01:31 +0300

    pyramid has been modified

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -29,7 +29,7 @@ function pyramid(n) {
   }
 }
 
-//second mode - using midpoint
+//second mode - using midpoint: O(N^2) runtime complexity
 function pyramid(n) {
 	const midpoint = Math.floor((2*n - 1) /2);
 	for(let row = 0; row < n; row++){

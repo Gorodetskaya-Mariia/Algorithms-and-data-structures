969c73d567677cd62104a01a3205fd29110a6246
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-02-03 18:03:55 +0300

    queue from stack has been solved

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -13,7 +13,47 @@
 //     q.remove(); // returns 2
 
 const Stack = require('./stack');
+const Stack1 = new Stack();
+const Stack2 = new Stack();
+class Queue {
+	constructor(){
+		this.Stack1 = new Stack();
+		this.Stack2 = new Stack();
+	}
 
-class Queue {}
+	add(record){
+		this.Stack1.push(record);
+	}
+
+	remove(){
+		while(this.Stack1.peek()){
+			const item = this.Stack1.pop();
+			this.Stack2.push(item);
+		}
+
+		const record = this.Stack2.pop();
+
+		while(this.Stack2.peek()){
+			this.Stack1.push(this.Stack2.pop());
+		}
+
+		return record;
+	}
+
+	peek(){
+		while(this.Stack1.peek()){
+			const item = this.Stack1.pop();
+			this.Stack2.push(item);
+		}
+
+		const record = this.Stack2.peek();
+
+		while(this.Stack2.peek()){
+			this.Stack1.push(this.Stack2.pop());
+		}
+
+		return record;
+	}
+}
 
 module.exports = Queue;

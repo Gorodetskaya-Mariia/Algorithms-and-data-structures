95e1d91030b996db493f1a01195e045afdb0e14a
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-05-08 18:14:09 +0300

    parseInt solution refactored

--- exercises/parseInt/index.js
+++ exercises/parseInt/index.js
@@ -12,32 +12,33 @@
 // The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
 
 function parseInt(string) {
-	const obj = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 };
-	const decade = { 'ten': 10, 'twenty': 20, 'thirty': 30, 'forty': 40, 'fifty': 50, 'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90 };
-	const obj1 = { 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19 };
+	const numbers = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 };
+	const bigNumbers = {
+		'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19,
+		'ten': 10, 'twenty': 20, 'thirty': 30, 'forty': 40, 'fifty': 50, 'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90
+	};
 	const s = string.split('-').join(' ').split(' ');
 	let res = 0, i = 0, label = true;
 
 	while (i < s.length) {
-		for (let char in obj) {
+		for (let char in numbers) {
 			if (char === s[i] && s.length > 3 && s[i + 1] === 'hundred' && res && res % 1000 == 0) {
-				res += obj[char] * 100;
+				res += numbers[char] * 100;
 				i++;
 				label = false;
 			} else if (char === s[i] && s.length > 1) {
-				res += obj[char];
+				res += numbers[char];
 			}
-			if (char === s[i] && s.length == 1) res += obj[char];
+			if (char === s[i] && s.length == 1) {
+				res += numbers[char];
+			}
+		}
+		for (let char in bigNumbers) {
+			if (char === s[i]) res += bigNumbers[char];
 		}
 		if (s[i] === 'hundred' && label) res *= 100;
 		if (s[i] === 'thousand') res *= 1000;
 		if (s[i] === 'million') res *= 1000000;
-		for (let char in decade) {
-			if (char === s[i]) res += decade[char];
-		}
-		for (let char in obj1) {
-			if (char === s[i]) res += obj1[char];
-		}
 		i++;
 	}
 	return res;
